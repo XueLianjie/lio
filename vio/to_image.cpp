976710f1ef07e1fcc2305e18143d94f435d6e97b
XueLianjie <Lianjie_Xue@outlook.com>
2022-12-10 17:08:52 +0800

    to image

--- vio/to_image.cpp
+++ vio/to_image.cpp
@@ -29,7 +29,7 @@ queue<sensor_msgs::PointCloud2ConstPtr> velodyne_que;
 ros::Publisher pub_image;
 ros::Publisher pub_cloud;
 static int i = 0;
-std::string dir = "/mnt/d/images";
+std::string dir = "/mnt/e/dataset/wangqiang/images";
 cv::Mat getImageFromMsg(const sensor_msgs::ImageConstPtr &img_msg) {
   cv_bridge::CvImageConstPtr ptr;
   ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::BGR8);
@@ -38,6 +38,10 @@ cv::Mat getImageFromMsg(const sensor_msgs::ImageConstPtr &img_msg) {
 }
 
 void image_callback(const sensor_msgs::ImageConstPtr &image_msg) {
+  if (i % 5 != 0) {
+    i++;
+    return;
+  }
   ROS_INFO("received image_msg time: %f ", image_msg->header.stamp.toSec());
   cv::Mat projected_image = getImageFromMsg(image_msg);
   cv::imwrite(dir + "/" + std::to_string(i) + ".png", projected_image);
@@ -50,7 +54,7 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "image_projection_node"); // node name
   ros::NodeHandle n("~"); // 指定子命名空间， launch中通过 ns =
   ros::Subscriber image_subscriber =
-      n.subscribe("/d435/camera/color/image_raw", 2000, image_callback);
+      n.subscribe("/camera/color/image_raw", 2000, image_callback);
   ros::spin();
   return 0;
 }
